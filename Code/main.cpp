9fd5c57142ecf0d4feb0a7f94b73dc09e5d8492d
Travis Collins <travisfcollins@gmail.com>
2012-02-12 18:13:04 -0500

    added phyical beginning outline files

---
+++ Code/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "all.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Main started\n";
+
+	//Call function to run example send/receive at phyiscal layer
+	physical_initial();
+
+	return 0;
+}
