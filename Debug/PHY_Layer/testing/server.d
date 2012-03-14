5058a9f4223b1797b1aff4fd4d658254f7dcb138
James Silvia <jamesesilvia@gmail.com>
2012-03-13 21:10:26 -0400

    fix my folder structure

--- Debug/PHY_Layer/testing/server.d
+++
@@ -1,4 +0,0 @@
-PHY_Layer/testing/server.d PHY_Layer/testing/server.o: \
- ../PHY_Layer/testing/server.cpp ../PHY_Layer/testing/all.h
-
-../PHY_Layer/testing/all.h:
--- Debug/PHY_Layer/testing/threads_example.d
+++
@@ -1,2 +0,0 @@
-PHY_Layer/testing/threads_example.d PHY_Layer/testing/threads_example.o: \
- ../PHY_Layer/testing/threads_example.cpp
