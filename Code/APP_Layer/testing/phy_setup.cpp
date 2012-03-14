59db44034d3cdd3aa08680b89a65a9f71f1c7764
jamesesilvia <jamesesilvia@gmail.com>
2012-03-14 10:00:23 -0300

    Update Code/APP_Layer/testing/phy_setup.cpp

--- Code/APP_Layer/testing/phy_setup.cpp
+++ Code/APP_Layer/testing/phy_setup.cpp
@@ -6,7 +6,7 @@
  *
  *      This function will setup sockets, and
  *      return the socket file descriptor
- *      returned by connect.
+ *      
  */
 
 #include "all.h"
@@ -20,19 +20,14 @@ int phy_setup(int port, struct hostent *serv){
 	//Initiate socket, diewithError
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock < 0) diewithError("socket() failed");
-
+	//Setup server address to connect
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	bcopy((char *) serv->h_addr, (char *) &serv_addr.sin_addr.s_addr,serv->h_length);
 	serv_addr.sin_port = htons(port);
-
+	//Connect to socket 
 	if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
 		diewithError("connect() failed");
 	}
-
 	return sock;
 }
-
-
-
-
