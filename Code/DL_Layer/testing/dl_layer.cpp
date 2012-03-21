0c12681de055878fd87aaecdccb36bc9815b2e90
Travis Collins <travisfcollins@gmail.com>
2012-03-21 18:45:32 -0400

    Added timers to DL_Layer

--- Code/DL_Layer/testing/dl_layer.cpp
+++ Code/DL_Layer/testing/dl_layer.cpp
@@ -16,15 +16,31 @@
  */
 
 #include "all.h"
+#include <sys/time.h>
+
 using namespace std;
+
+
+struct timeval tv;
+struct timezone tz;
+struct tm *tm;
 
 //Window Size
 #define MAX_SEQ 4
 #define MAX_PKT 200
+#define MAX_TIMEOUT 80000 //fix later
 
 #define PHY 1
 #define APP 2
 #define TIME_OUT 3
+
+//NOTES
+/*
+
+
+
+
+*/
 
 typedef struct{
 	int seq_NUM;
@@ -32,16 +48,33 @@ typedef struct{
 	char data[MAX_PKT];
 } frame;
 
+int timers[4]={0};
+
 static void send_data(int frame_to_send, int frame_expected, char buff);
 int wait_for_event(void);
 static bool between(int a, int b, int c);
+void *time_disp(void* num);
+in timeouts(void);
+
 
 int main(){
 	int frame_to_send = 0;
 	int frame_expected = 0;
 	int ack_expected = 0;
 	int queued = 0;
+	int rc;
 	frame buffer;
+
+	gettimeofday(&tv, &tz);
+	tm=localtime(&tv.tv_sec);
+	
+	//Spawn Timers Status Thread
+	pthread_t thread;
+        rc = pthread_create(&thread, NULL, time_disp , (void *) 1);
+	if (rc){
+		cout<<"Something bad happened with thread creation :("<<endl;
+		exit(1);
+	}
 
 	while (1) {
 
@@ -77,10 +110,13 @@ int main(){
 			//If No ACK received, and timeout
 			case (TIME_OUT):
 				frame_to_send = ack_expected;
+				//Reset N Frames
 				for (int i = 0; i <= queued; i++){
 					data = dl_send_q();
 					send_data(frame_to_send, frame_expected, data);
 					frame_to_send++;
+					//Reset Timer(s)
+					timers[i]=tv.tv_usec;
 					//clear the queue
 				}
 				break;
@@ -92,6 +128,8 @@ int main(){
 	} //while(1)
 } //main
 
+
+//Trigger when event occurs
 int wait_for_event(void){
 	 int event=0;
 
@@ -99,7 +137,7 @@ int wait_for_event(void){
         event=1;
     else if (!dl_send_q.empty())
         event=2;
-    else if (timeout)//Need a timeout function
+    else if (timeouts())//Need a timeout function
         event=3;
     else
         wait_for_event();
@@ -130,8 +168,28 @@ static bool between(int a, int b, int c){
 }
 
 
+//Check Timeout
+in timeouts(void){
 
+	current=tv.tv_usec;
+	//Look at times
+	for (int i=0;i<queued;i++)
+		if ((current-timers[i])>TIMEOUT_MAX)
+			return 1;//Timeout occured
 
+	return 0;//No timeouts
 
+}
 
+//Print out timers
+void *time_disp(void* num){
+	int old_times[4]={0};
+	//Update if times have changed
+	while(1)
+		if (old_time[0]!=timers[0] || old_time[1]!=timers[1] || old_time[2]!=timers[2] || old_time[3]!=timers[3]){
+			cout<<"Timers 1:"<<timers[0]<<"Timers 2:"<<timers[0]<<"Timers 3:"<<timers[0]<<"Timers 4:"<<timers[0]<<'\r'; 
+			for (int i=0;i<4;i++)
+				old_times[i]=timers[i];//Update old times
+		}
 
+}
